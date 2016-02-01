9b758946ec60b7560926f05bbbf01a6c39f54a98
Jérémy Levallois <jeremy.levallois@gmail.com>
2016-02-01 19:31:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# scholar.js
-Google Scholar parser to get your number of citations.
+# scholar.js shows dynamically your citation count on your publication list.
+
+## Aim
+
+Show your publication count for all your publications, from Google Scholar.
+
+## Requirements
+
+- Internet access (it asks [Google Scholar](http://scholar.google.com/) on page load).
+- [jQuery](http://jquery.com/).
+- custom [jQuery.ajax mid - CROSS DOMAIN AJAX](https://github.com/padolsey-archive/jquery.fn/tree/master/cross-domain-ajax) from @padolsey-archive (to load Google Scholar directly from js. Included inside scholar.js to remove dependance).
+
+## Usage
+
+Load results with your Google Scholar ID:
+
+```js
+Scholar.load("YOUR-GOOGLE-SCHOLAR-ID"); // Find it on your Google Scholar profile
+```
+
+Then, add span where you want to get your publication count:
+
+```html
+<span class="scholar" name="PUBLICATION-NAME" with-link="true"></span>
+```
+
+## Example
+
+```html
+<html>
+  <head>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <script type="text/javascript" src="js/scholar.js"></script>
+    <script type="text/javascript">
+      Scholar.load("-BL0_2EAAAAJ");
+    </script>
+  </head>
+  <body>
+    <p>Integral based Curvature Estimators in Digital Geometry -
+      <span class="scholar"
+            name="Integral based Curvature Estimators in Digital Geometry"
+            with-link="true">
+        <!-- add a loading image here -->
+      </span>
+    </p>
+  </body>
+</html>
+```
+
+### Result
+
+> Integral based Curvature Estimators in Digital Geometry - [12](https://scholar.google.fr/citations?view_op=view_citation&hl=fr&user=-BL0_2EAAAAJ&citation_for_view=-BL0_2EAAAAJ:u5HHmVD_uO8C)
+
+:octocat:
---
+++ js/scholar.js
@@ -0,0 +1,203 @@
+/**
+ * scholar.js
+ * ---
+ * @author Jérémy Levallois (http://www.karganys.fr)
+ * @version 0.1
+ * @updated 1-FEB-16
+ * ---
+ * Note: Read the README.md
+ * ---
+ * @info https://github.com/jlevallois/scholar.js/
+ */
+
+var Scholar = {
+  author: "SCHOLAR_AUTHOR_ID",
+  scholar_url: "http://scholar.google.com/",
+  debug: true,
+  publi_name : [],
+  publi_id : [],
+  publi_cite_count : [],
+  // preloaded : false,
+  load: function( _author ) {
+    this.author = _author;
+    if( this.debug ) {
+      console.log( "Preloading author " + this.author );
+    }
+
+    $.ajax({
+      url: Scholar.scholar_url + 'citations?user=' + Scholar.author + '&hl=en&cstart=0&pagesize=100',
+      type: 'GET',
+      dataType: 'html',
+      success: function(res) {
+        var tmp_find = $(res.responseText).find('#gsc_a_b');
+        var tmp_publi_array = $(tmp_find).find('.gsc_a_t').find('a');
+        var tmp_publi_cite = $(tmp_find).find('.gsc_a_c');
+
+        Scholar.publi_name = [];
+        Scholar.publi_id = [];
+        Scholar.publi_cite_count = [];
+
+        var regex_term = new RegExp('.*(citation_for_view=[\\d\\w\\-_]+\\:)');
+        for (i = 0; i < tmp_publi_array.length; i++) {
+          Scholar.publi_name.push( $(tmp_publi_array[i]).text() );
+          Scholar.publi_id.push( $(tmp_publi_array[i]).attr('href').replace(regex_term, '') );
+          Scholar.publi_cite_count.push( $(tmp_publi_cite[i]).text() );
+        }
+
+        if( Scholar.debug ) {
+          console.log( 'Found ' + Scholar.publi_name.length + ' publications' );
+
+          if( Scholar.publi_name.length != Scholar.publi_cite_count.length ) {
+            console.log( 'Wrong number of publications and cites count : ' + Scholar.publi_name.length  + ' and ' + Scholar.publi_cite_count.length);
+          }
+          else {
+            console.log('----------------------------------' );
+            for (i = 0; i < Scholar.publi_name.length; i++) {
+              console.log('Name: ' + Scholar.publi_name[i] );
+              console.log('ID: '   + Scholar.publi_id[i] );
+              console.log('Cite: ' + Scholar.publi_cite_count[i] );
+              console.log('----------------------------------' );
+            }
+          }
+        }
+
+        //////////////
+        var allElements = document.getElementsByClassName("scholar");
+        for (i = 0; i < allElements.length; i++) {
+          if( Scholar.debug ) {
+            console.log("Asking for: " + allElements[i].getAttribute("name"));
+          }
+          var found = false;
+          for( j = 0; j < Scholar.publi_name.length && !found; j++ ) {
+            if( Scholar.publi_name[j].toLowerCase() == allElements[i].getAttribute("name").toLowerCase() ) {
+              console.log('Found: Publication count ' + Scholar.publi_cite_count[j]);
+
+              if( allElements[i].getAttribute("with-link") == "true" ) {
+                allElements[i].innerHTML = '<a href="' + Scholar.scholar_url + 'citations?view_op=view_citation&hl=en&user=' + Scholar.author + '&citation_for_view=' + Scholar.author + ':' + Scholar.publi_id[j]  + '">' + Scholar.publi_cite_count[j] + '</a>';
+              }
+              else {
+                allElements[i].innerHTML = Scholar.publi_cite_count[j];
+              }
+              found = true;
+            }
+          }
+          if( !found )
+          {
+            allElements[i].innerHTML = '&#10008';
+          }
+        }
+      },
+      error: function() {
+        console.log('Can\'t open requested URL.');
+        var allElements = document.getElementsByClassName("scholar");
+        for (i = 0; i < allElements.length; i++) {
+          allElements[i].innerHTML = '&#10008';
+        }
+      },
+      complete: function(res) {
+        // Scholar.preloaded = true;
+      }
+    });
+  }//,
+  // print: function( _caller, _publi_name, _print_link, _author ) {
+  //   console.log("called");
+  //   if( arguments.length == 3 ) {
+  //     if( typeof this.preloaded === 'undefined' || !this.preloaded ){
+  //       setTimeout(function(){
+  //         Scholar.print( _caller, _publi_name, _print_link );
+  //       }, 250);
+  //     }
+  //     else {
+  //       var found = false;
+  //       for( i = 0; i < this.publi_name.length && !found; i++ ) {
+  //         if( this.publi_name[i].toLowerCase() == _publi_name.toLowerCase() ) {
+  //           console.log('Publication count ' + this.publi_cite_count[i]);
+  //           document.write('<div>' + this.publi_cite_count[i] + '</div>');
+  //           found = true;
+  //         }
+  //       }
+  //       console.log("B " + " " + _publi_name + " " + _print_link + " --- " + this.publi_name.length + " found : " + found);
+  //     }
+  //   }
+  //   else if( arguments.length == 4 ) {
+  //     // console.log("C " + _author + " " + _publi_name + " " + _print_link);
+  //   }
+  // }
+}
+
+
+/**
+ * jQuery.ajax mid - CROSS DOMAIN AJAX
+ * ---
+ * @author James Padolsey (http://james.padolsey.com)
+ * @version 0.11
+ * @updated 12-JAN-10
+ * ---
+ * Note: Read the README!
+ * ---
+ * @info http://james.padolsey.com/javascript/cross-domain-requests-with-jquery/
+ */
+
+jQuery.ajax = (function(_ajax){
+
+    var protocol = location.protocol,
+        hostname = location.hostname,
+        exRegex = RegExp(protocol + '//' + hostname),
+        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
+        query = 'select * from html where url="{URL}" and xpath="*"';
+
+    function isExternal(url) {
+        return !exRegex.test(url) && /:\/\//.test(url);
+    }
+
+    return function(o) {
+
+        var url = o.url;
+
+        if ( (typeof o.type === 'undefined' || /get/i.test(o.type)) && !/json/i.test(o.dataType) && isExternal(url) ) {
+
+            // Manipulate options so that JSONP-x request is made to YQL
+
+            o.url = YQL;
+            o.dataType = 'json';
+
+            o.data = {
+                q: query.replace(
+                    '{URL}',
+                    url + (o.data ?
+                        (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
+                    : '')
+                ),
+                format: 'xml'
+            };
+
+            // Since it's a JSONP request
+            // complete === success
+            if (!o.success && o.complete) {
+                o.success = o.complete;
+                delete o.complete;
+            }
+
+            o.success = (function(_success){
+                return function(data) {
+
+                    if (_success) {
+                        // Fake XHR callback.
+                        _success.call(this, {
+                            responseText: data.results[0]
+                                // YQL screws with <script>s
+                                // Get rid of them
+                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '').replace(/<img[^>]+?\/>|<img(.|\s)*?\/img>/gi, '')
+                        }, 'success');
+                    }
+
+                };
+            })(o.success);
+
+        }
+
+        return _ajax.apply(this, arguments);
+
+    };
+
+})(jQuery.ajax);
