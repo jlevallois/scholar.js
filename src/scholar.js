13d9909bdcc25372eb70245f21abbd8628665147
Jérémy Levallois <jeremy.levallois@liris.cnrs.fr>
2016-07-24 13:33:14 +0200

    fixing qunit issue + improving code quality.

--- src/scholar.js
+++ src/scholar.js
@@ -12,19 +12,19 @@
 
 
 var Scholar = {
-  author: 'SCHOLAR_AUTHOR_ID',
-  scholar_url: 'http://scholar.google.com/',
+  author: "SCHOLAR_AUTHOR_ID",
+  scholarURL: "http://scholar.google.com/",
   debug: false,
-  not_found_msg: '&#10008',
+  not_found_msg: "&#10008",
 
   load: function( _author ) {
-    'use strict';
+    "use strict";
     var i = 0,
         j = 0;
 
-    if( _author === 'SCHOLAR_AUTHOR_ID' ) {
+    if( _author === "SCHOLAR_AUTHOR_ID" ) {
       console.log( "You must specify your Google Scholar ID. See documentation." );
-      var allElements = document.getElementsByClassName("scholar");
+      var allElements = document.getElementsByClassName( "scholar" );
       for (i = 0; i < allElements.length; i++) {
         allElements[i].innerHTML = Scholar.not_found_msg;
       }
@@ -35,78 +35,78 @@ var Scholar = {
     }
 
     $.ajax({
-      url: Scholar.scholar_url + 'citations?user=' + Scholar.author + '&hl=en&cstart=0&pagesize=100',
-      type: 'GET',
-      dataType: 'html',
+      url: Scholar.scholarURL + "citations?user=" + Scholar.author + "&hl=en&cstart=0&pagesize=100",
+      type: "GET",
+      dataType: "html",
       success: function(res) {
-        var tmp_find = $(res.responseText).find('#gsc_a_b');
-        var tmp_publi_array = $(tmp_find).find('.gsc_a_t').find('a');
-        var tmp_publi_cite = $(tmp_find).find('.gsc_a_c');
+        var tmpFind = $(res.responseText).find( "#gsc_a_b" );
+        var tmpPubliArray = $(tmpFind).find( ".gsc_a_t" ).find( "a" );
+        var tmp_publi_cite = $(tmpFind).find( ".gsc_a_c" );
 
-        var publi_name = [];
-        var publi_id = [];
-        var publi_cite_count = [];
+        var publiName = [];
+        var publiId = [];
+        var publiCiteCount = [];
 
-        var regex_term = new RegExp('.*(citation_for_view=[\\d\\w\\-_]+\\:)');
-        for (i = 0; i < tmp_publi_array.length; i++) {
-          publi_name.push( $(tmp_publi_array[i]).text() );
-          publi_id.push( $(tmp_publi_array[i]).attr('href').replace(regex_term, '') );
-          publi_cite_count.push( $(tmp_publi_cite[i]).text() );
+        var regexTerm = new RegExp( ".*(citation_for_view=[\\d\\w\\-_]+\\:)" );
+        for (i = 0; i < tmpPubliArray.length; i++) {
+          publiName.push( $(tmpPubliArray[i]).text() );
+          publiId.push( $(tmpPubliArray[i]).attr( "href").replace(regexTerm, "") );
+          publiCiteCount.push( $(tmp_publi_cite[i]).text() );
         }
 
         if( Scholar.debug ) {
-          console.log( 'Found ' + publi_name.length + ' publications' );
+          console.log( "Found " + publiName.length + " publications." );
 
-          if( publi_name.length !== publi_cite_count.length ) {
-            console.log( 'Wrong number of publications and cites count : ' + publi_name.length  + ' and ' + publi_cite_count.length);
+          if( publiName.length !== publiCiteCount.length ) {
+            console.log( "Wrong number of publications and cites count : " + publiName.length  + " and " + publiCiteCount.length);
           }
           else {
-            console.log('----------------------------------' );
-            for (i = 0; i < publi_name.length; i++) {
-              console.log('Name: ' + publi_name[i] );
-              console.log('ID: '   + publi_id[i] );
-              console.log('Cite: ' + publi_cite_count[i] );
-              console.log('----------------------------------' );
+            console.log( "----------------------------------" );
+            for (i = 0; i < publiName.length; i++) {
+              console.log( "Name: " + publiName[i] );
+              console.log( "ID: "   + publiId[i] );
+              console.log( "Cite: " + publiCiteCount[i] );
+              console.log( "----------------------------------" );
             }
           }
         }
 
-        var allElements = document.getElementsByClassName("scholar");
+        var allElements = document.getElementsByClassName( "scholar" );
         for( i = 0; i < allElements.length; i++ ) {
           var pos = -1;
-          if( allElements[i].getAttribute("publi-id") ) {
+          if( allElements[i].getAttribute( "publi-id" )) {
             if( Scholar.debug ) {
-              console.log("Asking for publication ID: " + allElements[i].getAttribute("publi-id"));
+              console.log( "Asking for publication ID: " + allElements[i].getAttribute( "publi-id" ));
             }
-            for( j = 0; j < publi_id.length && pos === -1; j++ ) {
-              if( publi_id[j] === allElements[i].getAttribute("publi-id") ) {
+            for( j = 0; j < publiId.length && pos === -1; j++ ) {
+              if( publiId[j] === allElements[i].getAttribute( "publi-id" )) {
                 pos = j;
               }
             }
           }
-          else if( allElements[i].getAttribute("name") ) {
+          else if( allElements[i].getAttribute( "name" )) {
             if( Scholar.debug ) {
-              console.log("Asking for: " + allElements[i].getAttribute("name"));
+              console.log( "Asking for: " + allElements[i].getAttribute( "name" ));
             }
-            for( j = 0; j < publi_name.length && pos === -1; j++ ) {
-              if( publi_name[j].toLowerCase().replace(/[^a-zA-Z]+/g, '') === allElements[i].getAttribute("name").toLowerCase().replace(/[^a-zA-Z]+/g, '') ) {
+            for( j = 0; j < publiName.length && pos === -1; j++ ) {
+              if( publiName[j].toLowerCase().replace(/[^a-zA-Z]+/g, '') === allElements[i].getAttribute("name").toLowerCase().replace(/[^a-zA-Z]+/g, '') ) {
                 pos = j;
               }
             }
           }
           else {
             if( Scholar.debug ) {
-              console.log("No attribute 'publi-id' or 'name' found for this publication.");
+              console.log( "No attribute 'publi-id' or 'name' found for this publication." );
             }
           }
 
           if( pos !== -1 ) {
-            var count = (publi_cite_count[pos] < 1)?0:publi_cite_count[pos];
+            var count = (publiCiteCount[pos] < 1)?0:publiCiteCount[pos];
             if( Scholar.debug ) {
-              console.log('Publication found. Count: ' + count);
+              console.log( "Publication found. Count: " + count );
             }
-            if( allElements[i].getAttribute("with-link") === "true" ) {
-              allElements[i].innerHTML = '<a href="' + Scholar.scholar_url + 'citations?view_op=view_citation&hl=en&user=' + Scholar.author + '&citation_for_view=' + Scholar.author + ':' + publi_id[pos]  + '">' + count + '</a>';
+            if( allElements[i].getAttribute( "with-link" ) === "true" ) {
+              allElements[i].innerHTML = "<a href=\"" + Scholar.scholarURL + "citations?view_op=view_citation&hl=en&user=" + Scholar.author + "&citation_for_view=" + Scholar.author + ":" + publiId[pos]  + "\">" + count + "</a>";
             }
             else {
               allElements[i].innerHTML = count;
@@ -119,10 +119,10 @@ var Scholar = {
       },
       error: function() {
         if( Scholar.debug ) {
-          console.log('Can\'t open requested URL.');
+          console.log( "Can't open requested URL." );
         }
-        var allElements = document.getElementsByClassName("scholar");
-        for (i = 0; i < allElements.length; i++) {
+        var allElements = document.getElementsByClassName( "scholar" );
+        for( i = 0; i < allElements.length; i++ ) {
           allElements[i].innerHTML = Scholar.not_found_msg;
         }
       }
@@ -143,12 +143,12 @@ var Scholar = {
  */
 
 jQuery.ajax = (function(_ajax){
-    'use strict';
+    "use strict";
     var protocol = location.protocol,
         hostname = location.hostname,
-        exRegex = new RegExp(protocol + '//' + hostname),
-        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
-        query = 'select * from html where url="{URL}" and xpath="*"';
+        exRegex = new RegExp(protocol + "//" + hostname),
+        YQL = "http" + (/^https/.test(protocol)?'s':'') + "://query.yahooapis.com/v1/public/yql?callback=?",
+        query = "select * from html where url=\"{URL}\" and xpath=\"*\"";
 
     function isExternal(url) {
         return !exRegex.test(url) && /:\/\//.test(url);
@@ -158,26 +158,26 @@ jQuery.ajax = (function(_ajax){
 
         var url = o.url;
 
-        if ( (typeof o.type === 'undefined' || /get/i.test(o.type)) && !/json/i.test(o.dataType) && isExternal(url) ) {
+        if ( (typeof o.type === "undefined" || /get/i.test(o.type)) && !/json/i.test(o.dataType) && isExternal(url) ) {
 
             // Manipulate options so that JSONP-x request is made to YQL
 
             o.url = YQL;
-            o.dataType = 'json';
+            o.dataType = "json";
 
             o.data = {
                 q: query.replace(
-                    '{URL}',
+                    "{URL}",
                     url + (o.data ?
                         (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
                     : '')
                 ),
-                format: 'xml'
+                format: "xml"
             };
 
             // Since it's a JSONP request
             // complete === success
-            if (!o.success && o.complete) {
+            if( !o.success && o.complete ) {
                 o.success = o.complete;
                 delete o.complete;
             }
@@ -192,7 +192,7 @@ jQuery.ajax = (function(_ajax){
                                 // YQL screws with <script>s
                                 // Get rid of them
                                 .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '').replace(/<img[^>]+?\/>|<img(.|\s)*?\/img>/gi, '')
-                        }, 'success');
+                        }, "success" );
                     }
 
                 };
